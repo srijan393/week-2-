cfe5c2e0677a92203cc3a55f5ae72381515836bc
srijan393 <146333937+srijan393@users.noreply.github.com>
2023-10-05 13:25:14 +0100

    answer for portfolio question 4

---
+++ portfolio4.py
@@ -0,0 +1,8 @@
+sweet=input("enter no.of sweets")
+pupil=input("enter number of pupil")
+sweet=int(sweet)
+pupil=int(pupil)
+a= sweet/pupil
+a=int(a)
+print("sweets given to each pupil",a)
+print("no of sweets left are",sweet%pupil)
